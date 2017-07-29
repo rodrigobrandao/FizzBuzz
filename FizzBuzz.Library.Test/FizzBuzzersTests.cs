25c23384b3d1fdbf295be4bcc625e06a7f851e3b
Rodrigo Brandao <rodrigo.brandao@gmail.com>
2017-07-29 15:31:04 -0300

    Add project files.

---
+++ FizzBuzz.Library.Test/FizzBuzzersTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Test
+{
+    [TestFixture]
+    public class FizzBuzzersTests
+    {
+        [Test]
+        public void Buzzer_WhenDefaults_ReturnsInput([Values(1,2,4,7,8,11,13,14,16,17)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual(input.ToString(), output);
+        }
+                
+        [Test]
+        public void Buzzer_When3_ReturnsFizz()
+        {
+            string output = FizzBuzzer.GetValue(3);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void Buzzer_When5_ReturnsBuzz([Values(5,10,20)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3,6,9,12,18)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDiv3AndDiv5_ReturnsFizzBuzz([Values(15,30)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzz.Library.FizzBuzzer.GetValue(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
